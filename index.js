13980b63e4f7eddb3bf942c6a64f8c396c666f2f
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-07-16 23:55:27 -0700

    feat: gets it working
    Adds basic functionality, still needs testing

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-crypto = require("crypto");
-fs = require('fs/promises');
-path = require('path')
+const crypto = require("crypto");
+const fs = require('fs/promises');
+const path = require('path');
+
+const unified = require('unified');
+const parseHtml = require('rehype-parse');
+const visit = require('unist-util-visit');
+const isScriptTag = require('hast-util-is-javascript');
+const isStyleTag = require('hast-util-is-css-style')
+const isCssLink = require('hast-util-is-css-link')
 
 module.exports= {
-    onPostBuild: async function({constants}) {
-        console.log(await getHtmlFilesFromDir(constants.PUBLISH_DIR));
+    onPostBuild: async function({constants, utils}) {
+        try {
+            const htmlFiles = await getHtmlFilesFromDir(constants.PUBLISH_DIR)
+            const cspHashesPromises = htmlFiles.map(file => processHtmlFile(file));
+            const cspHashes = await Promise.all(cspHashesPromises);
+            const redirectStrings = cspHashes.map((hashData) => generateRedirectString(hashData, constants.PUBLISH_DIR));
+           await fs.appendFile(path.join(constants.PUBLISH_DIR, '_headers'), redirectStrings.join(' '))  
+           utils.status.show({
+               title: 'Source Hashing Completed Successfully',
+               summary: `${htmlFiles.length} Files Processed. ${cspHashes.reduce((numHashesCounted, currentHash) => {
+                   numHashesCounted += currentHash.hashes.script.length + currentHash.hashes.style.length
+               }, 0)}`,
+               text: cspHashes.map((hash) => `file: ${hash.filePath}, number of script tags hashed: ${hash.hashes.script.length}, number of style tags hashed: ${hash.hashes.style.length}`).join('\n')
+           }) 
+        } catch (error) {
+            utils.build.failPlugin(error.message);
+        }
     }
 }
 
@@ -23,7 +45,7 @@ async function getHtmlFilesFromDir(directory) {
     // the output array of html files
     const htmlFiles = [];
     // searches all files in the current directory. If html, add to htmlFiles, otherwise if directory recurse. 
-    for(i = 0; i< filesInCurrentDir.length; i++) {
+    for(let i = 0; i< filesInCurrentDir.length; i++) {
         const currentFile = filesInCurrentDir[i];
         if(currentFile.isDirectory()) {
             dirPromises.push(getHtmlFilesFromDir(path.join(directory, currentFile.name)))
@@ -37,4 +59,36 @@ async function getHtmlFilesFromDir(directory) {
         htmlFiles.push(...fileList);
     });
     return htmlFiles;
+}
+
+async function processHtmlFile(filePath) {
+    const file = await fs.readFile(filePath, {encoding: 'utf-8'})
+    const ast = unified().use(parseHtml).parse(file);
+    const hashes = visitNode(ast);
+    return {filePath: filePath, hashes}
+}
+
+function visitNode(tree) {
+    const hashLists = {script: [], style: []}
+    visit(tree, [isScriptTag, isStyleTag, isCssLink] , function(node) {
+        if(!node.properties.src && node.tagName !== 'link') {
+        const sha256Hash = crypto.createHash('sha256')
+        sha256Hash.update(node.children[0].value)
+        hashLists[node.tagName].push(`sha256-${sha256Hash.digest('hex')}`)
+        } else if(node.properties.src) {
+            hashLists[node.tagName].push(node.properties.src);
+        } else if(node.tagName === 'link') {
+            hashLists.style.push(node.properties.href);
+        }
+    })
+    return hashLists
+}
+
+
+function generateRedirectString({filePath, hashes}, publishPath) {
+const url = filePath.replace(publishPath, '').replace(/^\/index.html/, '/');
+return (
+`${url}
+    Content-Security-Policy: script-src ${hashes['script'].join(' ')}; style-src ${hashes['style'].join(' ')};
+`)
 }
