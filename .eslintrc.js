72ffb56bc4d00e05a330261f03cb011f11d8b1f4
Darrik <30670444+mdarrik@users.noreply.github.com>
2020-07-16 23:54:46 -0700

    chore: adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2020": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+        "no-unused-vars": "warn"
+    }
+};
